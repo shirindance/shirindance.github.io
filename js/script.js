ca87a6538a363232df09609dbde9a703d5caec35
shirindance <shirindancecompany@gmail.com>
2026-07-15 17:19:54 +0200

    Pulizia repository per redesign

--- js/script.js
+++
@@ -1,5 +0,0 @@
-// Quando clicchi sul bottone, mostra un messaggio
-document.getElementById("salutoBtn").addEventListener("click", function() {
-  alert("Ciao! Hai cliccato il bottone 😄");
-});
-
--- js/videohover.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.video-box').forEach(box => {
-      const video = box.querySelector('video');
-      box.addEventListener('mouseenter', () => { video.play(); }); // parte il video
-      box.addEventListener('mouseleave', () => { video.pause(); video.currentTime = 0; }); // si ferma e torna all'inizio
-    })
